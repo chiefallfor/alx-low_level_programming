7d4054074987cb35a192c08a9d1002dbc6b28726
chiefallfor <osonduoffor02@gmail.com>
2023-02-23 03:05:25 -0800

    lower case of alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,15 +4,12 @@
  *
  * Return: always o;
  */
-
-
-
-
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char i;
-
 	for (i = 'a' ; i <= 'z' ; i++)
+	{
 		_putchar(i);
 	_putchar('\n');
+	}
 }
